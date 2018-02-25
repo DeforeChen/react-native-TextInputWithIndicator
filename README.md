568e98341d70e92aaa73c115c8cebe74d7d325aa
Chen Defore <Defore@ChendeMacBook-Pro.local>
2018-02-25 21:07:40 +0800

    Update 1. readme 2. indicateMode

--- README.md
+++ README.md
@@ -4,22 +4,31 @@ A customized TextInput component with input length indicator.
 ## Description
 This component aims at providing a way to show the limit text length and the text length that has already been inputted at the right corner of an TextInput area.
 
-You can almost regard the component as a extension of reac-native TextInput component.
+You can almost regard the component as a extension of `reac-native` TextInput component.
 
-## Usage
+## Effects
+
+![Img1](https://upload-images.jianshu.io/upload_images/1180547-cdce1216d2694d6f.gif?imageMogr2/auto-orient/strip%7CimageView2/2/w/316)
+
+![Img](https://upload-images.jianshu.io/upload_images/1180547-8b0ec4de7a48d4a6.gif?imageMogr2/auto-orient/strip%7CimageView2/2/w/316)
+
+## Props
 | Props | Type | Description |
-| --- | :-: | :-: |
+| --- | :-- | :-- |
 | areaWidth | number | width of component |
 | areaHeight | number | height of component |
 | placeholder | string | placeholder text |
 | maxLength | number | tell component the limit length of your input |
 | onChangeText | func | received the text change event |
+| indicateMode | number |`indicateMode.leftMode`: indicator will display the left length to input.(see the effects image2) |
+|||`indicateMode.alreadyInputMode`: example: indicator will display `1/60`  |
 
 ## Demo
 
 ``` js
 // Omit part of the code
 // ...
+import {TextInputWithIndicator,indicateMode} from './TextInputWithIndicator';
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -29,10 +38,12 @@ export default class App extends Component<Props> {
                 <Text style={styles.instructions}>
                     Welcome to use
                 </Text>
-                <TextInputWithIndicator areaHeight={100} maxLength={90} areaWidth={width} placeholder={'please input'}
-                                        onChangeText={(t)=>{console.log(t)}}
-
+                <View style={{width:width,height:1,backgroundColor:'black'}}/>
+                <TextInputWithIndicator areaHeight={100} maxLength={60} areaWidth={width} placeholder={'please input'}
+                                        indicateMode={indicateMode.leftMode}
+                                        onChangeText={(t)=>{console.log(t)}} autoFocus={true}
                 />
+                <View style={{width:width,height:1,backgroundColor:'black'}}/>
             </View>
         );
     }
--- TextInputWithIndicator.js
+++ TextInputWithIndicator.js
@@ -14,10 +14,17 @@ import {
 import PropTypes from 'prop-types';
 
 const indicatorHeight = 30;
-export default class TextInputWithIndicator extends PureComponent {
+
+export const indicateMode = {
+    leftMode: 0, // 剩余多少输入
+    alreadyInputMode: 1, //已经输入多少了
+};
+
+export class TextInputWithIndicator extends PureComponent {
     constructor(props) {
         super(props);
-        this.state = {inputTxtLen: 0,};
+        this.state = {textLength: this.props.indicateMode === indicateMode.alreadyInputMode ? 0 : this.props.maxLength};
+        this._onInput = this._onInput.bind(this);
     }
 
     static propTypes = {
@@ -26,32 +33,49 @@ export default class TextInputWithIndicator extends PureComponent {
         placeholder: PropTypes.string,
         maxLength: PropTypes.number.isRequired,
         onChangeText: PropTypes.func,
+        indicateMode: PropTypes.number.isRequired,
+        autoFocus: PropTypes.bool,
+    };
+
+    _onInput = (text) => {
+        let maxLen = this.props.maxLength;
+
+        this.props.onChangeText(text);
+        if (this.props.indicateMode === indicateMode.alreadyInputMode) {
+            // 针对ios 原生的中文九宫格输入作的长度边界处理
+            if (text.length < maxLen) {
+                this.setState({textLength: text.length});
+            } else {
+                if (this.state.textLength !== maxLen) {
+                    this.setState({textLength: maxLen});
+                }
+            }
+        } else {
+            if (maxLen - text.length <= 0) {
+                if (this.state.textLength !== 0) {
+                    this.setState({textLength: 0});
+                }
+            } else {
+                this.setState({textLength: maxLen - text.length});
+            }
+        }
     };
 
     render() {
         let maxLen = this.props.maxLength;
-        let txtLen = this.state.inputTxtLen;
+        let txtLen = this.state.textLength;
+        let templateText = this.props.indicateMode === indicateMode.alreadyInputMode ? `${txtLen}/${maxLen}` : `${txtLen}`;
 
         return (
             <View style={[styles.container, {width: this.props.areaWidth, height: this.props.areaHeight}]}>
                 <TextInput style={[styles.inputArea, {height: this.props.areaHeight}]}
-                           autoFocus={true} multiline={true}
+                           multiline={true} autoFocus={this.props.autoFocus}
                            placeholder={this.props.placeholder} maxLength={maxLen}
-                           onChangeText={(text) => {
-                               this.props.onEditText(text);
-                               // 针对ios 原生的中文九宫格输入作的长度边界处理
-                               if (text.length < maxLen) {
-                                   this.setState({inputTxtLen: text.length});
-                               } else {
-                                   if (this.state.inputTxtLen !== maxLen) {
-                                       this.setState({inputTxtLen: maxLen});
-                                   }
-                               }
-                           }}
+                           onChangeText={this._onInput}
                 />
                 <View style={[styles.indicatorArea, {width: this.props.areaWidth}]}>
                     <Text style={styles.indicatorText}>
-                        {`${txtLen}/${maxLen}`}
+                        {templateText}
                     </Text>
                 </View>
             </View>
